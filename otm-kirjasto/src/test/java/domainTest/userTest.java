ece50e62bef6381145a5d7a77352b0fb69bf3a71
Aleksander Matikainen <AM@dsl-hkibng32-54f84c-229.dhcp.inet.fi>
2018-05-01 15:47:23 +0300

    viikko6

--- otm-kirjasto/src/test/java/domainTest/userTest.java
+++ otm-kirjasto/src/test/java/domainTest/userTest.java
@@ -1,6 +1,5 @@
 package domainTest;
 
-
 import dao.UserDao;
 import domain.Book;
 import domain.User;
@@ -20,44 +19,41 @@ public class userTest {
 
     @Before
     public void setUp() throws Exception {
-        
+
     }
 
     @After
     public void tearDown() {
-    
+
     }
-    
+
     @Test
     public void equalWhenSame() {
         User u1 = new User("aleKus", "uu8fJ1");
         User u2 = new User("aleKus", "uu8fJ1");
         assertTrue(u1.equals(u2));
     }
-    
+
     @Test
-    public void nonEqualWhenNotSame() {
+    public void nonEqualWhenNotSame() { 
         User u1 = new User("aleKus", "uu8fJ1");
         User u2 = new User("allu", "uu8fJ1");
         assertFalse(u1.equals(u2));
     }
-    
-    @Test
-    public void nonEqualsWhenDifferentTypes() {
-        try {
-            User u1 = userDao.findByUsername("user1");
-            String st = "w";
-            assertFalse(u1.equals(new Book(0, st, st, 0)));
 
-        } catch (SQLException ex) {
-            Logger.getLogger(userTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    @Test
+    public void nonEqualsWhenDifferentTypes() { 
+
+        User u1 = new User("jj", "lsdd3");
+        String st = "w";
+        assertFalse(u1.equals(new Book(0, st, st, 0)));
+
     }
-    
+
     @Test
     public void toStringOK() {
         User u1 = new User("allu", "1111");
         assertEquals(u1.toString(), "allu, 1111");
     }
-    
+
 }
